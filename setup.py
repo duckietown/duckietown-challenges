0cc41230741e09bdd324b89bb0d5c3dca8afaef6
Andrea Censi <acensi@ethz.ch>
2020-03-08 19:18:11 +0100

    fix-branch-name

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_version(filename):
 
 
 version = get_version(filename='src/duckietown_challenges/__init__.py')
-line = 'daffy'
+line = 'daffy-aido4'
 setup(name=f'duckietown-challenges-{line}',
       version=version,
       download_url='http://github.com/duckietown/duckietown-challenges/tarball/%s' % version,
