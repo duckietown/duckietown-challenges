598324da574305175f3a9ab4b3d74c0eeea37590
Andrea Censi <acensi@ethz.ch>
2020-10-29 13:42:29 +0100

    fix

--- src/duckietown_challenges/challenge.py
+++ src/duckietown_challenges/challenge.py
@@ -13,7 +13,7 @@ from .cmd_submit_build import parse_complete_tag
 from .exceptions import InvalidConfiguration
 from .utils import indent, safe_yaml_dump, wrap_config_reader2
 
-__all__ = ["ChallengeDescription"]
+__all__ = ["ChallengeDescription", "EvaluationParameters"]
 
 
 class InvalidChallengeDescription(Exception):
